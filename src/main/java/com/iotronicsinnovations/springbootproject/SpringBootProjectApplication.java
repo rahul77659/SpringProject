13dd18f54111a811b13a34b588e5de55605aefe5
Rahul <rahulgaurav77659@gmail.com>
2024-06-03 18:19:42 +0530

    GET Product by id FAKESTOREAPI

--- src/main/java/com/iotronicsinnovations/springbootproject/SpringBootProjectApplication.java
+++ src/main/java/com/iotronicsinnovations/springbootproject/SpringBootProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-atuspublic class SpringBootProjectApplication {
+public class SpringBootProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProjectApplication.class, args);
