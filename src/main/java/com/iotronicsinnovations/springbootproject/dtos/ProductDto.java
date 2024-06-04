f03ce6fe1505ce48b54119235f6bf520502b1180
Rahul <rahulgaurav77659@gmail.com>
2024-06-04 20:05:50 +0530

    Create a product API

--- src/main/java/com/iotronicsinnovations/springbootproject/dtos/ProductDto.java
+++ src/main/java/com/iotronicsinnovations/springbootproject/dtos/ProductDto.java
@@ -1,4 +1,16 @@
 package com.iotronicsinnovations.springbootproject.dtos;
 
 public class ProductDto {
+/*id:31,
+                title:'...',
+                price:'...',
+                category:'...',
+                description:'...',
+                image:'..
+                */
+    private Long id;
+    private int price;
+    private String Category;
+    private String description;
+    private String image;
 }
